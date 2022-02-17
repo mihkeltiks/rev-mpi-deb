876495c37ee849d3ffcd6791a7ee3fc9cb0d1c94
Ott Martens <ott.martens@pipedrive.com>
2022-02-17 14:23:03 +0200

    update mpi dummy example

--- examples/hello_mpi_c/hello.c
+++ examples/hello_mpi_c/hello.c
@@ -5,9 +5,7 @@
 int size;
 int rank;
 
-int number;
-
-int main(int argc, char **argv)
+void stuff()
 {
     MPI_Init(NULL, NULL);
 
@@ -16,24 +14,43 @@ int main(int argc, char **argv)
 
     printf("Hello world from processor rank %d\n", rank);
 
-    if (size < 2)
-    {
-        printf("Too few processes to do message passing. exiting\n");
-        MPI_Finalize();
-        exit(0);
-    }
+    int sendNumber = 123;
+    int recvNumber;
+    
+    MPI_Send(&sendNumber, 1, MPI_INT, rank, 0, MPI_COMM_WORLD);
+    printf("%d: sending value %d to %d\n", rank, sendNumber, rank);
+    
+    MPI_Recv(&recvNumber, 1, MPI_INT, rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("%d: received value %d from %d\n", rank, recvNumber, rank);
 
-    if (rank == 0)
-    {
-        number = 123;
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        printf("%d: sending value %d to %d\n", rank, number, 1);
-    }
-    else if (rank == 1)
-    {
-        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("%d: received value %d from %d\n", rank, number, 0);
-    }
+    // if (size < 2)
+    // {
+    //     printf("Too few processes to do message passing. exiting\n");
+    //     MPI_Finalize();
+    //     exit(0);
+    // }
+
+    // if (rank == 0)
+    // {
+    //     number = 123;
+    //     MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    //     printf("%d: sending value %d to %d\n", rank, number, 1);
+    // }
+    // else if (rank == 1)
+    // {
+    //     MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    //     printf("%d: received value %d from %d\n", rank, number, 0);
+    // }
 
     MPI_Finalize();
+}
+
+void does()
+{
+    stuff();
+}
+
+int main(int argc, char **argv)
+{
+    does();
 }
