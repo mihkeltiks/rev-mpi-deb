06e0580846113d354dbd8d9c46a0e026947a89d6
Ott Martens <ott.martens@pipedrive.com>
2022-03-19 11:44:41 +0200

    Add proper logger

--- src/debugger/logger/logger.go
+++ src/debugger/logger/logger.go
@@ -7,13 +7,80 @@ import (
 
 type color string
 
+type LoggingLevel int
+
+var MAX_LOG_LEVEL = debug
+
+var Levels levelsStruct = levelsStruct{
+	Err:     err,
+	Warn:    warn,
+	Info:    info,
+	Verbose: verbose,
+	Debug:   debug,
+}
+
 const (
-	cyan  color = "\033[36m"
-	reset color = "\033[0m"
+	reset  color = "\033[0m"
+	cyan   color = "\033[36m"
+	blue   color = "\033[34m"
+	yellow color = "\033[33m"
+	red    color = "\033[31m"
 )
 
+const (
+	err     LoggingLevel = 1
+	warn    LoggingLevel = 2
+	info    LoggingLevel = 3
+	verbose LoggingLevel = 4
+	debug   LoggingLevel = 5
+)
+
+type levelsStruct struct {
+	Err     LoggingLevel
+	Warn    LoggingLevel
+	Info    LoggingLevel
+	Verbose LoggingLevel
+	Debug   LoggingLevel
+}
+
+var colorMap map[LoggingLevel]color = map[LoggingLevel]color{
+	err:   red,
+	warn:  yellow,
+	info:  cyan,
+	debug: blue,
+}
+
+func SetMaxLogLevel(level LoggingLevel) {
+	MAX_LOG_LEVEL = level
+}
+
+func Error(str string, args ...interface{}) {
+	logRow(err, str, args...)
+}
+
+func Warn(str string, args ...interface{}) {
+	logRow(warn, str, args...)
+}
+
 func Info(str string, args ...interface{}) {
-	fmt.Print(cyan)
+	logRow(info, str, args...)
+}
+
+func Verbose(str string, args ...interface{}) {
+	logRow(info, str, args...)
+}
+
+func Debug(str string, args ...interface{}) {
+	logRow(debug, str, args...)
+}
+
+func logRow(level LoggingLevel, str string, args ...interface{}) {
+
+	if level > MAX_LOG_LEVEL {
+		return
+	}
+
+	fmt.Print(colorMap[level])
 	fmt.Print(timeString())
 	fmt.Printf(str, args...)
 	fmt.Println(reset)
